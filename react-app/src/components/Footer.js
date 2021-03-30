b245be9c90107625b0522e96dd0a75d65d1ed59f
Steve McNamara <stephenmcnamara36@gmail.com>
2021-03-30 13:18:45 -0600

    made footer match navbar

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <div className="card">
+      <div className="footer-dark bg-secondary">
         <div className="card-body" id="footer">
           <p>&copy; 2021 Steve McNamara</p>
         </div>
